9cc254935941366be3737270150d1d6f4778c6de
Eileen <39421738+minteapixel@users.noreply.github.com>
2019-10-30 12:44:59 -0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,3 @@
-// module.exports = {
-//   mongoURI:
-//   'mongodb://minty:rustykawaiix3@ds145895.mlab.com:45895/expenses-react-mtp',
-//   secret: 'asl28934lknsdflis2df',
-//   cookieKey: 'df984hf9md?!@kdf'
-// };
-
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
